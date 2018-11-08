d215df3ddcfe9d3069f8e1ba67b67c0f51b6c86f
Jastria Rahmat <ijash1000@gmail.com>
2018-11-08 18:05:16 +0700

    merge view, add error pages
    added error pages and home view background zoom edit.

--- routes/views.js
+++ routes/views.js
@@ -27,6 +27,30 @@ router.get('/register', async (req, res) => {
     loginAction: "/api/auth",
     colorTheme: temaWarna
   });
+
+});
+router.get('/error/:code', async (req, res) => {
+  statusCodeList = {
+    301: 'Moved Permanently',
+    400: 'Bad Request',
+    403: 'Forbidden',
+    404: 'Not Found',
+    500: 'Internal Server Error',
+    502: 'Bad Gateway',
+    503: 'Service Unavailable',
+    504: 'Gateway Timeout',
+    501: 'Not Implemented'
+  };
+  let currentStatus = req.params.code
+  if (!statusCodeList[req.params.code]) currentStatus = '501' ,req.query.details = `${req.params.code} status code is not implemented yet`
+  if (!req.query.details) req.query.details = "we're not sure what was just happened..."
+  res.status(req.params.code).render('./error',{
+    status:currentStatus,
+    statusDefinition:statusCodeList[currentStatus] ,
+    errorDetails:req.query.details,
+    judul:judulWebsite,
+    colorTheme: temaWarna
+  });
 });
 
 module.exports = router;
