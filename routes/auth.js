4dd13d1e83fb919a2695fb1e659a042c03266622
Jastria Rahmat <ijash1000@gmail.com>
2018-11-10 01:59:05 +0700

    perbaiki error page

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,13 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);//kirim pug
+  if (error) return res.redirect('../error/400?details='+error.details[0].message);//kirim pug //buatin ajax di view
    
   let user = await User.findOne({email:req.body.email});
-  if (!user) return res.status(400).send('Invalid email or password');
+  if (!user) return res.redirect('../error/404?details=invalid username or password...');
 
   const validPassword = await bcrypt.compare(req.body.password, user.password)
-  if (!user) return res.redirect('../error/404?details=invalid username or password...');
+  if (!validPassword) return res.redirect('../error/404?details=invalid username or password...');
   
   const token = user.generateAuthToken(); 
   res.send(token);// buat pug
--- routes/views.js
+++ routes/views.js
@@ -2,34 +2,29 @@ const pug = require('pug');
 const express = require('express');
 const router = express.Router();
 
-judulWebsite = 'SEWAKAPAL'
-temaWarna = 'blue'
-router.get('/', async (req, res) => {
-
-  res.render('./home/index',{
-  // res.render('./home/template-materialize',{
-    judul:judulWebsite,
+let defaultSiteValues = {
+    judul:'B.E.B.E.',
     kategoriKapal:"#",
     registrasi:"/register",
     loginAction: "/api/auth",
-    colorTheme: temaWarna
+    colorTheme: 'blue'
+  }
+
+router.get('/', async (req, res) => {
+  const pageVariables = Object.assign(defaultSiteValues,{
   });
+  res.render('./home/index',pageVariables);
 });
 
 router.get('/register', async (req, res) => {
-
-  res.render('./register',{
-  // res.render('./home/template-materialize',{
-    judul:judulWebsite,
-    kategoriKapal:"#",
-    regisAction: "/api/users/",
-    registrasi:"/register",
-    loginAction: "/api/auth",
-    colorTheme: temaWarna
+  const pageVariables = Object.assign(defaultSiteValues,{
+    regisAction: "/api/users/"
   });
-
+  res.render('./register',pageVariables);
 });
+
 router.get('/error/:code', async (req, res) => {
+  let currentStatus = req.params.code
   statusCodeList = {
     301: 'Moved Permanently',
     400: 'Bad Request',
@@ -41,16 +36,18 @@ router.get('/error/:code', async (req, res) => {
     504: 'Gateway Timeout',
     501: 'Not Implemented'
   };
-  let currentStatus = req.params.code
+
+  
   if (!statusCodeList[req.params.code]) currentStatus = '501' ,req.query.details = `${req.params.code} status code is not implemented yet`
   if (!req.query.details) req.query.details = "we're not sure what was just happened..."
-  res.status(req.params.code).render('./error',{
+  
+  const pageVariables = Object.assign(defaultSiteValues,{
     status:currentStatus,
     statusDefinition:statusCodeList[currentStatus] ,
-    errorDetails:req.query.details,
-    judul:judulWebsite,
-    colorTheme: temaWarna
+    errorDetails:req.query.details
   });
+
+  res.status(req.params.code).render('./error',pageVariables);
 });
 
 module.exports = router;
